a01829c48d4660b9e89ab58b96008ea4a1711cbc
saravanan290702 <76428931+saravanan290702@users.noreply.github.com>
2022-05-20 16:01:05 +0530

    init

---
+++ bot.py
@@ -0,0 +1,43 @@
+import discord
+import random
+from discord.ext import commands
+intents = discord.Intents(messages=True, guilds=True)
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.....')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency*1000)}ms')
+
+@client.event
+async def on_message(message):
+      M=message.content.lower()
+      if 'good morning proto' in M:
+            ans= ['good morning!','hello gm']
+            await message.channel.send(random.choice(ans))
+      if 'good afternon proto' in M:
+            ans= ['good afternoon!','hy','sup']
+            await message.channel.send(random.choice(ans))
+      if 'good night proto' in M:
+            ans= ['gn','good night!','cya']
+            await message.channel.send(random.choice(ans))
+      if 'lol proto' in M:
+            ans= ['huh!','grrr','get lost']
+            await message.channel.send(random.choice(ans))
+      if 'hello proto' in M or 'hy proto' in M or 'hi proto' in M or 'halo proto' in M or 'helo proto' in M:
+            ans= ['hii!','hello','sup bud','sup homie','hmm! you called for me']
+            await message.channel.send(random.choice(ans))
+      if 'what did you eat proto' in M:
+            ans= ['pizza','noodles','sushi :yum:']
+            await message.channel.send(random.choice(ans)) 
+      if 'how you doing proto' in M:
+            ans= ['sleeping','ntg much','playing','eating','yer mom']
+            await message.channel.send(random.choice(ans))      
+
+
+client.run('ODAxMDQyMTgxNjI4NDkzODk2.YAa6qg.tw83ja8N9dLyOQ7rm85VMY2AqH4')
